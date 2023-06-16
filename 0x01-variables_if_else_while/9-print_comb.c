ad13669ef84afd7de41db7c334a01458a94120e1
root <mutungamuuo777@gmail.com>
2023-06-16 13:29:52 -0700

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single-digit numbers
+ *              separated by ", " in ascending order using putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = i; j < 10; j++)
+        {
+            putchar(i + '0');
+            putchar(j + '0');
+
+            if (i != 9 || j != 9)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
+}
+
