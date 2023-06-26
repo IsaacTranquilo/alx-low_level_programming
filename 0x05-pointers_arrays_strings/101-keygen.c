e37778d1f26f9b99aa497cd5330acc555043d821
root <mutungamuuo777@gmail.com>
2023-06-26 12:13:29 -0700

    commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,37 +4,42 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
- * generate_password - Generates a random valid password.
- * @length: The desired length of the password.
- *
- * Return: The generated password as a dynamically allocated string.
- */
-char *generate_password(int length)
-{
-    static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-    char *password = malloc((length + 1) * sizeof(char));
-    int i;
+#define PASSWORD_LENGTH 10
 
-    for (i = 0; i < length; i++) {
-        int index = rand() % (sizeof(charset) - 1);
-        password[i] = charset[index];
+/**
+ * generate_random_password - Generates a random valid password.
+ *
+ * Return: The generated password.
+ */
+char *generate_random_password()
+{
+    static const char valid_characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int i;
+    char *password = malloc((PASSWORD_LENGTH + 1) * sizeof(char));
+
+    if (password == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(1);
     }
 
-    password[length] = '\0';
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
+        int random_index = rand() % (sizeof(valid_characters) - 1);
+        password[i] = valid_characters[random_index];
+    }
+
+    password[PASSWORD_LENGTH] = '\0';
+
     return password;
 }
 
 int main(void)
 {
-    int password_length = 8;
     char *password;
-
     srand(time(NULL));
 
-    password = generate_password(password_length);
-    printf("Generated Password: %s\n", password);
-    free(password);
+    password = generate_random_password();
+    printf("Generated password: %s\n", password);
 
+    free(password);
     return 0;
 }
