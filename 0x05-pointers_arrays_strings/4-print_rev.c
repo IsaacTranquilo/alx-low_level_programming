9bb91602e8176f29162284a6334fc63cdfd53de6
root <mutungamuuo777@gmail.com>
2023-06-26 11:01:52 -0700

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,11 @@ void print_rev(char *s)
 {
     int length = 0;
 
-    // Calculate the length of the string
-    while (s[length] != '\0')
+   while (s[length] != '\0')
     {
         length++;
     }
 
-    // Print the string in reverse
     for (int i = length - 1; i >= 0; i--)
     {
         putchar(s[i]);
