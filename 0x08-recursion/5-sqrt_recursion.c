541daf610ccf115c5a82b2a3ce59d20d243a0150
root <mutungamuuo777@gmail.com>
2023-07-05 11:07:27 -0700

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+ * _sqrt_helper - Recursive helper function to find the square root
+ * @n: The number to calculate the square root of
+ * @start: The starting point for the search
+ * @end: The ending point for the search
+ *
+ * Return: The square root of n if found, or -1 otherwise
+ */
+int _sqrt_helper(int n, int start, int end)
+{
+	if (start > end)
+		return (-1);
+
+	int mid = start + (end - start) / 2;
+	int sqrt = mid * mid;
+
+	if (sqrt == n)
+		return (mid);
+	else if (sqrt < n)
+		return (_sqrt_helper(n, mid + 1, end));
+	else
+		return (_sqrt_helper(n, start, mid - 1));
+}
+
+/**
  * _sqrt_recursion - Returns the natural square root of a number
  * @n: The number to calculate the square root of
  *
@@ -12,36 +36,5 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 
-	if (n == 0 || n == 1)
-		return (n);
-
-	return (_sqrt_helper(n, 1, n));
-}
-
-/**
- * _sqrt_helper - Recursive helper function to find the square root
- * @n: The number to calculate the square root of
- * @start: The starting point for the search
- * @end: The ending point for the search
- *
- * Return: The square root of n if found, or -1 otherwise
- */
-int _sqrt_helper(int n, int start, int end)
-{
-	int mid, sqrt;
-
-	if (start <= end)
-	{
-		mid = (start + end) / 2;
-		sqrt = mid * mid;
-
-		if (sqrt == n)
-			return (mid);
-		else if (sqrt > n)
-			return (_sqrt_helper(n, start, mid - 1));
-		else
-			return (_sqrt_helper(n, mid + 1, end));
-	}
-
-	return (-1);
+	return (_sqrt_helper(n, 0, n));
 }
