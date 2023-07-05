e9582a450b110e17e696d0e1eaf0edf958cbe359
root <mutungamuuo777@gmail.com>
2023-07-05 13:13:44 -0700

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,38 +3,26 @@
 /**
  * sqrt_helper - Helper function to find the square root recursively.
  * @n: The number for which to find the square root.
- * @start: The starting point of the search range.
- * @end: The ending point of the search range.
+ * @guess: The current guess for the square root.
  *
- * Return: The square root of the number within the search range, if found, otherwise -1.
+ * Return: The square root of the number if it exists, otherwise -1.
  */
-int sqrt_helper(int n, int start, int end)
+int sqrt_helper(int n, int guess)
 {
     /* Base case: If the number is negative, it does not have a natural square root. */
     if (n < 0)
         return -1;
 
-    /* Base case: If the number is 0 or 1, the square root is the number itself. */
-    if (n == 0 || n == 1)
-        return n;
+    /* Base case: If the guess squared is equal to n, return the guess as the square root. */
+    if ((guess * guess) == n)
+        return guess;
 
-    /* Base case: If the search range is exhausted, return -1. */
-    if (start > end)
+    /* Base case: If the guess squared is greater than n, the square root doesn't exist. */
+    if ((guess * guess) > n)
         return -1;
 
-    /* Calculate the middle value of the search range. */
-    int mid = start + (end - start) / 2;
-
-    /* Check if the square of the middle value is equal to the number n. */
-    if ((mid * mid) == n)
-        return mid;
-
-    /* If the square of the middle value is greater than n, search the lower half of the range. */
-    if ((mid * mid) > n)
-        return sqrt_helper(n, start, mid - 1);
-
-    /* If the square of the middle value is less than n, search the upper half of the range. */
-    return sqrt_helper(n, mid + 1, end);
+    /* Recursive case: Increment the guess and call the helper function recursively. */
+    return sqrt_helper(n, guess + 1);
 }
 
 /**
@@ -45,5 +33,5 @@ int sqrt_helper(int n, int start, int end)
  */
 int _sqrt_recursion(int n)
 {
-    return sqrt_helper(n, 0, n);
+    return sqrt_helper(n, 0);
 }
