d63ace33a6e62871188666ce2a9c4a4bc493e695
root <mutungamuuo777@gmail.com>
2023-07-05 11:28:19 -0700

    commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,38 +1,34 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * wildcmp - Compares two strings with support for wildcards (*)
- * @s1: The first string
- * @s2: The second string
+ * wildcmp - Compares two strings and checks for wildcard match
+ * @s1: First string to compare
+ * @s2: Second string to compare (can contain wildcards)
  *
- * Return: 1 if the strings can be considered identical, 0 otherwise
+ * Return: 1 if the strings can be considered identical, otherwise return 0
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0')
-        return (1);
+	int match;
 
-    if (*s1 == *s2)
-        return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+			return (1);
+		match = wildcmp(s1, s2 + 1);
+		if (*(s1 + 1) != '\0' && match == 0)
+			match = wildcmp(s1 + 1, s2);
+	}
+	else if (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (1);
+		match = wildcmp(s1 + 1, s2 + 1);
+	}
+	else
+	{
+		return (0);
+	}
 
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) != '\0' && *s1 == '\0')
-            return (0);
-
-        return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-    }
-
-    return (0);
-}
-
-int main(void)
-{
-    char *s1 = "hello";
-    char *s2 = "h*o";
-    int result = wildcmp(s1, s2);
-    printf("Are the strings identical? %s\n", result ? "Yes" : "No");
-
-    return (0);
+	return (match);
 }
