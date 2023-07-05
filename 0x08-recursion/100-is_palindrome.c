a4414e6e01471823ff2645139bf1eb2b682ba1f7
root <mutungamuuo777@gmail.com>
2023-07-05 11:32:49 -0700

    commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,60 +1,27 @@
 #include "main.h"
 
 #include <stdio.h>
-
-/**
- * _strlen - Calculates the length of a string
- * @s: The string
- *
- * Return: The length of the string
- */
-int _strlen(char *s)
-{
-	int len = 0;
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-	return (len);
-}
-
-/**
- * is_palindrome_helper - Recursive helper function to check if a string is a palindrome
- * @s: The string
- * @start: The starting index
- * @end: The ending index
- *
- * Return: 1 if the string is a palindrome, 0 otherwise
- */
-int is_palindrome_helper(char *s, int start, int end)
-{
-	if (start >= end)
-		return (1);
-
-	if (s[start] != s[end])
-		return (0);
-
-	return (is_palindrome_helper(s, start + 1, end - 1));
-}
+#include <string.h>
 
 /**
  * is_palindrome - Checks if a string is a palindrome
- * @s: The string
+ * @s: The string to check
  *
  * Return: 1 if the string is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-	int len = _strlen(s);
-	return (is_palindrome_helper(s, 0, len - 1));
-}
+    int i, j;
+    int len = strlen(s);
 
-int main(void)
-{
-    char *s = "level";
-    int result = is_palindrome(s);
-    printf("Is the string a palindrome? %s\n", result ? "Yes" : "No");
+    if (len == 0)
+        return (1);
 
-    return (0);
+    for (i = 0, j = len - 1; i < j; i++, j--)
+    {
+        if (s[i] != s[j])
+            return (0);
+    }
+
+    return (1);
 }
