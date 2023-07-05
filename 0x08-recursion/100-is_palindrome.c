048c357af827be205fcfb66e281fb70de3b07b5c
root <mutungamuuo777@gmail.com>
2023-07-05 11:59:48 -0700

    commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,9 +9,9 @@
 int _strlen(char *s)
 {
         if (*s == '\0')
-                return 0;
+                return (0);
         else
-                return 1 + _strlen(s + 1);
+                return (1 + _strlen(s + 1));
 }
 
 /**
@@ -26,13 +26,13 @@ int _is_palindrome(char *s, int start, int end)
 {
         /* Base case: Empty string or single character is a palindrome. */
         if (start >= end)
-                return 1;
+                return (1);
 
         /* Check if the characters at start and end indices match. */
         if (s[start] == s[end])
-                return _is_palindrome(s, start + 1, end - 1);
+                return (_is_palindrome(s, start + 1, end - 1));
         else
-                return 0;
+                return (0);
 }
 
 /**
@@ -44,5 +44,5 @@ int _is_palindrome(char *s, int start, int end)
 int is_palindrome(char *s)
 {
         int len = _strlen(s);
-        return _is_palindrome(s, 0, len - 1);
+        return (_is_palindrome(s, 0, len - 1));
 }
