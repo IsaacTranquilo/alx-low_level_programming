edaa6ed78d65f39087c01878e3c62432e1ff9b4a
root <mutungamuuo777@gmail.com>
2023-07-07 11:47:35 -0700

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,29 +1,17 @@
 #include "main.h"
-
 #include <stdio.h>
-#include <string.h>
-
 /**
  * main - Entry point of the program
- *
- * Description: This program prints its name, including the path, followed by a new line.
- * If the program is renamed, it will still print the new name without needing to recompile.
+ * @argc: The number of command-line arguments passed
+ * @argv: An array of pointers to the arguments
  *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-    char *programName;
-
-    /* Get the program name from the argument */
-    programName = strrchr(argv[0], '/');
-    if (programName == NULL)
-        programName = argv[0];
-    else
-        programName++; /* Move past the '/' character */
-
-    /* Print the program name */
-    printf("%s\n", programName);
-
-    return (0);
+        (void)argc; /* Unused parameter */
+        printf("%s\n", argv[0]);
+        return (0);
 }
+
+
