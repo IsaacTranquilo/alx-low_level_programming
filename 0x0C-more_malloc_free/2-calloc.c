1ee9ecee0735de333827469d2ccefa465474377b
IsaacTranquilo <mutungamuuo777@gmail.com>
2023-07-14 11:03:40 -0700

    commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,16 +15,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int total_size;
 
 	if (nmemb == 0 || size == 0)
-		return (NULL);
+		return NULL;
 
 	total_size = nmemb * size;
-	ptr = malloc(total_size);
 
+	ptr = malloc(total_size);
 	if (ptr == NULL)
-		return (NULL);
+		return NULL;
 
 	/* Set memory to zero using memset */
 	memset(ptr, 0, total_size);
 
-	return (ptr);
+	return ptr;
 }
